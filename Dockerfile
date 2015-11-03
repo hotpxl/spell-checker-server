8d5066b062b0c695df448ceca3d9a7d6dc49c2d6
Yutian Li <hotpxless@gmail.com>
2015-11-03 15:34:19 -0800

    [master] put together server

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,15 @@ FROM ubuntu:trusty
 RUN echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections && \
     apt-get --yes update && apt-get --yes install build-essential g++ flex \
     bison gperf ruby perl libsqlite3-dev libfontconfig1-dev libicu-dev git \
-    libfreetype6 libssl-dev libpng-dev libjpeg-dev python libx11-dev libxext-dev
+    libfreetype6 libssl-dev libpng-dev libjpeg-dev python libx11-dev \
+    libxext-dev python python-dev python-pip
+COPY . /spell-checker-server
+RUN cd spell-checker-server && pip install -r requirements.txt
 RUN git clone git://github.com/ariya/phantomjs.git && \
     cd phantomjs && \
     git checkout 2.0.0 && \
-    ./build.sh  --confirm
+    ./build.sh  --confirm && \
+    mv bin/phantomjs /spell-checker-server
+
+EXPOSE 8080
+CMD ["bin/bash"]
--- server.py
+++ server.py
@@ -1,10 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from __future__ import absolute_import
 from __future__ import print_function
 import os.path
-
+import flask
 import spell_checker
 
+app = flask.Flask(__name__)
+
+app.config.update(
+    JSONIFY_PRETTYPRINT_REGULAR=False
+)
+
+checker = spell_checker.GoogleSpellChecker(os.path.abspath('./phantomjs'))
+
+@app.route('/spell-check', methods=['POST'])
+def spell_check():
+    if not flask.request.json or 'word' not in flask.request.json:
+        return (flask.jsonify({'error': 'unrecognized format'}), 400)
+    else:
+        res = checker.correct_spelling(flask.request.json['word'])
+        return flask.jsonify({'result': res})
 
 if __name__ == '__main__':
+    app.run()
 
-    a = spell_checker.GoogleSpellChecker(os.path.abspath('./phantomjs'))
