1b8ebec9f54c9a60a8f21956bf7fe87cf567f87b
Yutian Li <hotpxless@gmail.com>
2016-07-20 01:45:09 +0800

    [master] Fix tar command.

--- get-phantomjs.sh
+++ get-phantomjs.sh
@@ -6,5 +6,5 @@ pushd "${file_path}" > /dev/null
 
 mkdir -p ./server/bin
 curl --fail --location --remote-header-name --remote-name "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2"
-tar xzf phantomjs-2.1.1-linux-x86_64.tar.bz2
+tar xf phantomjs-2.1.1-linux-x86_64.tar.bz2
 cp ./phantomjs-2.1.1-linux-x86_64/bin/phantomjs ./server/bin/
